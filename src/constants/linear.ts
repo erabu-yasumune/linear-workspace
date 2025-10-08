5f09a574556c80ee1e8f357c23a70b2bfb0a1865
erabu-yasumune <erabu@rakko.jp>
2025-10-09 08:04:05 +0900

    定数修正

--- src/constants/linear.ts
+++ src/constants/linear.ts
@@ -6,6 +6,6 @@
  * Available estimate values for Linear issues
  * Based on Fibonacci sequence commonly used in agile estimation
  */
-export const ESTIMATE_OPTIONS = [0, 1, 2, 3, 4, 8, 13, 21] as const;
+export const ESTIMATE_OPTIONS = [0, 1, 2, 3, 5, 8, 13, 21] as const;
 
 export type EstimateValue = (typeof ESTIMATE_OPTIONS)[number];
